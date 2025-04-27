7577c97599ce7cf2634e2c7edc07a9fbf3f97b99
Daniel Russell <druss16@gmail.com>
2025-04-27 14:00:31 -0400

    Updated entrypoint.sh deux

--- edgr_invest/entrypoint.sh
+++ edgr_invest/entrypoint.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --noinput || {
 
 # Start Gunicorn
 echo "Starting Gunicorn..."
-exec gunicorn edgr_project.wsgi:application --bind 0.0.0.0:$PORT --timeout 600 --workers 1 --worker-class gevent
+exec gunicorn edgr_invest.wsgi:application --bind 0.0.0.0:$PORT --timeout 600 --workers 1 --worker-class gevent
