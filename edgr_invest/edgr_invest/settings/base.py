78fd37b667ca3d02deefeff07906a98e5eb7dca4
Daniel Russell <druss16@gmail.com>
2025-07-06 17:04:58 -0400

    Updated STATS DIR

--- edgr_invest/edgr_invest/settings/base.py
+++ edgr_invest/edgr_invest/settings/base.py
@@ -214,7 +214,10 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "frontend", "assets"),  # Add frontend/assets/
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
