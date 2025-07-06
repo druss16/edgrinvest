f7d5b3159db894e8b9092fd8a12de4541022aa84
Daniel Russell <druss16@gmail.com>
2025-07-06 17:20:08 -0400

    Updated Frontend Static Folder

--- edgr_invest/edgr_invest/settings/base.py
+++ edgr_invest/edgr_invest/settings/base.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -213,10 +215,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
+import os
+
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "frontend", "assets"),  # Add frontend/assets/
+    os.path.join(BASE_DIR, "frontend", "build"),  # Add React build directory
+    os.path.join(BASE_DIR, "frontend", "src", "assets"),  # Include raw assets if needed
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
