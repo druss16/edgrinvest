38534bd25787ea6b23468b85052fa013b6bb1723
Daniel Russell <druss16@gmail.com>
2025-07-14 13:59:52 -0400

    Updated admin

--- edgr_invest/users/admin.py
+++ edgr_invest/users/admin.py
@@ -22,8 +22,6 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
-# users/admin.py
-
 from django.contrib import admin
 from .models import UserProfile
 
@@ -46,6 +44,3 @@ class UserProfileAdmin(admin.ModelAdmin):
         except Exception:
             return f"User ID {obj.user_id}"
     user_display.short_description = 'User'
-
-    def get_queryset(self, request):
-        return super().get_queryset(request).select_related('user_id')
